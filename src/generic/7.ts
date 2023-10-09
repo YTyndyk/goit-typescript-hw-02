a135a9dd089ad9f90b154fc46bf97aaa252eb8da
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-10-09 21:10:46 +0300

    finish task 8

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,16 +4,18 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+	admin = "admin",
+	editor = "editor",
+	guest = "guest",
 }
 
 // Замініть наступний код на версію за допомогою Record
+type UserRoleDescripyion = Record<UserRole, string>;
+
 const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+	admin: "Admin User",
+	editor: "Editor User",
+	guest: "Guest User",
 };
 
 export {};
